510a69455b1558634dfe88f7e835f0cdd5510899
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-10-19 19:17:39 -0600

    Delete products from cart

--- MainCourse/models/cart.js
+++ MainCourse/models/cart.js
@@ -39,6 +39,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
       updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
@@ -48,4 +51,16 @@ module.exports = class Cart {
       });
     });
   }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    })
+  }
 };
+
