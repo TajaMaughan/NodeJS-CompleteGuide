b0b5724c3c2a12f5a6547782219a2b7423f957a7
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-10-22 18:15:44 -0600

    finished mysql, started sequelize integration

--- MainCourse/utils/database.js
+++ MainCourse/utils/database.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: process.env.password
-});
-module.exports = pool.promise();
+const sequelize = new Sequelize('node-complete', 'root', process.env.password, { dialect: 'mysql', host: 'localhost' });
+
+module.exports = sequelize;
