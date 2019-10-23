bbb75f20d12b3f1268cbaed7977d317f56d15577
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-10-23 11:14:12 -0600

    started user mode

---
+++ MainCourse/models/user.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allnowNull: false,
+    primaryKey: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
