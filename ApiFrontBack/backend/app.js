ca80da6dc16eaad058a91a2717cb9db016387ef5
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-12-15 18:57:34 -0700

    User signup

--- ApiFrontBack/backend/app.js
+++ ApiFrontBack/backend/app.js
@@ -58,11 +58,12 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
-app.unsubscribe((error, req, res, next) => {
+app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
